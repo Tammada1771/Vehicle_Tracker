a541deaf06c4c448bb4e006d07aad49b90aae1e4
Adam Tamminga <500158560@fvtc.edu>
2021-02-04 19:54:00 -0600

    Added the PL and EF files

---
+++ ART.VehicleTracker/ART.VehicleTracker.PL/TblColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ART.VehicleTracker.PL
+{
+    public partial class tblColor
+    {
+        public tblColor()
+        {
+            tblVehicles = new HashSet<tblVehicle>();
+        }
+
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public int Code { get; set; }
+
+        public virtual ICollection<tblVehicle> tblVehicles { get; set; }
+    }
+}
---
+++ ART.VehicleTracker/ART.VehicleTracker.PL/TblModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ART.VehicleTracker.PL
+{
+    public partial class tblModel
+    {
+        public tblModel()
+        {
+            tblVehicles = new HashSet<tblVehicle>();
+        }
+
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<tblVehicle> tblVehicles { get; set; }
+    }
+}
