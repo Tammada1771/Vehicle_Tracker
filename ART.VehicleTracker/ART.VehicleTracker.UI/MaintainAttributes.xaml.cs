9fc4bb5bf66cd3177090e967bf20a4ab56439592
Adam Tamminga <500158560@fvtc.edu>
2021-02-13 18:12:35 -0600

    Added UI windows

---
+++ ART.VehicleTracker/ART.VehicleTracker.UI/MaintainAttributes.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ART.VehicleTracker.UI
+{
+    /// <summary>
+    /// Interaction logic for MaintainAttributes.xaml
+    /// </summary>
+    public partial class MaintainAttributes : Window
+    {
+        public MaintainAttributes()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnInsert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CboAttribute_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ART.VehicleTracker/ART.VehicleTracker.UI/VehicleList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ART.VehicleTracker.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class VehicleList : Window
+    {
+        public VehicleList()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnColors_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnMakes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnModels_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnNewVehicle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditVehicle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
