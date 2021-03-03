2edfb7684b122d160dad27cba36ecfaa71471d26
Adam Tamminga <500158560@fvtc.edu>
2021-03-02 20:06:56 -0600

    More work on the API

---
+++ ART.VehicleTracker/ART.VehicleTracker.API/Controllers/VehicleController.cs
@@ -0,0 +1,106 @@
+﻿using ART.VehicleTracker.BL;
+using ART.VehicleTracker.BL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ART.VehicleTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        // GET: api/<VehicleController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vehicle>>> Get()
+        {
+            try
+            {
+                return Ok(await VehicleManager.Load());
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{colorName}")]
+        public async Task<ActionResult<IEnumerable<Vehicle>>> Get(string colorName)
+        {
+            //Return all vehicles
+            try
+            {
+                return Ok(await VehicleManager.Load(colorName));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // GET api/<VehicleController>/5
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult<IEnumerable<Vehicle>>> Get(Guid id)
+        {
+            try
+            {
+                return Ok(await VehicleManager.LoadById(id));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // POST api/<VehicleController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Vehicle vehicle)
+        {
+            try
+            {
+                return Ok(await VehicleManager.Insert(vehicle));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        // PUT api/<VehicleController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] Vehicle vehicle)
+        {
+            try
+            {
+                return Ok(await VehicleManager.Update(vehicle));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // DELETE api/<VehicleController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                return Ok(await VehicleManager.Delete(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
